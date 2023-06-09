50e364dd1b993562d9cc4c1f1401d3b236f5aeea
koloseki <koloseki2002@gmail.com>
2023-06-09 19:46:44 +0200

    Styled a website and added a modal for new folder

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+let Folders = [];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const OpenNewFolderModal = document.querySelector('.newFolder')
+const CloseNewFolderModal = document.querySelector('.closeNewFolderModal')
+const FolderModal = document.querySelector('.folderModal');
+const Modal = document.querySelector('dialog')
+
+
+
+OpenNewFolderModal.addEventListener("click", () => {
+    FolderModal.showModal();
+    console.log("test1");
+})
+
+CloseNewFolderModal.addEventListener("click", () => {
+    FolderModal.close();
+})
+
+Modal.addEventListener("click" , e =>{
+  const dialogDimensions = Modal.getBoundingClientRect();
+  if(
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ){
+    Modal.close()
+  }
+})
